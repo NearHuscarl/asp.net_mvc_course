437cbf21c7a665cb9a894c0a09677c5676e109bb
NearHuscarl <near.huscarl@gmail.com>
2020-04-29 11:14:20 +0700

    first commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(_context.Customers.Include(c => c.MembershipType).ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.Where(c => c.ID == id).FirstOrDefault();
+
+            if (customer == null) return HttpNotFound();
+
+            return View(customer);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _context.Dispose();
+        }
+    }
+}
