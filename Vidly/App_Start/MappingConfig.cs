b42da0477dae84ce248756eb268d44e66731a50c
NearHuscarl <near.huscarl@gmail.com>
2020-05-01 12:46:39 +0700

    add web api exercise sln

---
+++ Vidly/App_Start/MappingConfig.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingConfig
+    {
+        public static IMapper Mapper { get; private set; }
+
+        public static void Initialize()
+        {
+            var config = new MapperConfiguration(x =>
+            {
+                x.CreateMap<Customer, CustomerDto>();
+                x.CreateMap<CustomerDto, Customer>().ForMember(m => m.ID, o => o.Ignore());
+                x.CreateMap<Movie, MovieDto>();
+                x.CreateMap<MovieDto, Movie>().ForMember(m => m.ID, o => o.Ignore());
+            });
+
+            Mapper = config.CreateMapper();
+        }
+    }
+}
---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.App_Start;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/customers
+        public IHttpActionResult GetCustomers()
+        {
+            var customers = _context.Customers.ToList().Select(MappingConfig.Mapper.Map<Customer, CustomerDto>);
+
+            return Ok(customers);
+        }
+
+        // GET /api/customers/1
+        public IHttpActionResult GetCustomers(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.ID == id);
+
+            if (customer == null)
+                NotFound();
+
+            return Ok(MappingConfig.Mapper.Map<Customer, CustomerDto>(customer));
+        }
+
+        // POST /api/customers
+        [HttpPost]
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var customer = MappingConfig.Mapper.Map<CustomerDto, Customer>(customerDto);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            customerDto.ID = customer.ID; // customer.ID has been set by the database at this point
+
+            return Created(new Uri(Request.RequestUri + "/" + customer.ID), customerDto);
+        }
+
+        // PUT /api/customers/1
+        [HttpPut]
+        public IHttpActionResult UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.ID == id);
+
+            if (customerInDb == null)
+                return NotFound();
+
+            MappingConfig.Mapper.Map(customerDto, customerInDb);
+            _context.SaveChanges();
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE /api/customers/1
+        [HttpDelete]
+        public IHttpActionResult DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.ID == id);
+
+            if (customerInDb == null)
+                return NotFound();
+
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
